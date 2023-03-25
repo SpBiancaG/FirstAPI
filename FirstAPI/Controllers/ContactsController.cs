91299a7928ae7c1e288dd4b41ada7c529c5a22c1
SpBiancaG <spbiancag@gmail.com>
2023-03-25 14:09:18 +0200

    Add project files.

---
+++ FirstAPI/Controllers/ContactsController.cs
@@ -0,0 +1,51 @@
+﻿using FirstAPI.Data;
+using FirstAPI.Models;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : Controller
+    {
+        private readonly ContactsAPIDbContext dbContext;
+
+        public ContactsController(ContactsAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        
+        
+        
+        //[] face adnotare
+        [HttpGet]
+        public async Task<IActionResult> GetContacts()
+        {
+            return Ok(await dbContext.Contacts.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task< IActionResult> AddContact(AddContactRequest addContactRequest)
+        {
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                Adress = addContactRequest.Address,
+                Email = addContactRequest.Email,
+                FullName = addContactRequest.FullName,
+                Phone = addContactRequest.Phone
+            };
+
+           await  dbContext.Contacts.AddAsync(contact);
+           await dbContext.SaveChangesAsync();
+
+           
+           return Ok(contact);   
+        }
+
+
+        
+    }
+}
---
+++ FirstAPI/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FirstAPI.Models;
+namespace FirstAPI.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
